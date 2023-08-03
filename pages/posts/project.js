2b0aa0309f04c6856ad83153ebbfd3b41bdc3724
Ravi Patel <rp70960@gmail.com>
2023-08-03 19:23:07 -0400

    Render Project Page

---
+++ pages/posts/project.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+export default function ProjectPost() {
+    return (
+    <>
+    <Head>
+    <title>Project</title>
+          <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>My Project</h1>
+    </>
+    );
+  }
