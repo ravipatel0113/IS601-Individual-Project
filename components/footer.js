cb8292fe2da7d5bd8d0b7b1851fa507f37dc391a
Ravi Patel <rp70960@gmail.com>
2023-08-06 01:11:43 -0400

    attempt with mailchimp signup form

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faBook, faBullhorn, faEnvelope, faPhone, faStore, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-import NewsLetterSignUpForm from './subcribe_newsletter';
 
 import { useState } from "react";
 export default function Footer() {
@@ -67,7 +66,52 @@ export default function Footer() {
             <a href="#"><FontAwesomeIcon icon={faPhone} /> Contact</a>
 
         </div>
-        <NewsLetterSignUpForm />
+        <div id="mc_embed_shell">
+                <Link href="/cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" ></Link>
+            <div id="mc_embed_signup">
+                <form action="https://njit.us9.list-manage.com/subscribe/post?u=3d0b19ed396d6468d588f214e&amp;id=366234b116&amp;v_id=4559&amp;f_id=001013e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                    <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+                        
+                        <div className="mc-field-group">
+                            <label for="mce-EMAIL">
+                                Email Address:
+                            </label>
+                            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required="" >
+
+                            </input>
+                            <span id="mce-EMAIL-HELPERTEXT" className="helper_text">
+
+                            </span>
+                        </div>
+                        <div id="mergeRow-gdpr" className="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+                            <div className="content__gdpr">
+                                <p>Please select all the ways you would like to hear from Ravi:</p>
+                                <fieldset className="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+                                    <label className="checkbox subfield" for="gdpr40083">
+                                        <input type="checkbox" id="gdpr_40083" name="gdpr[40083]" className="gdpr" ></input>
+                                        <span>Email</span>
+                                    </label>
+                                </fieldset>
+                            </div>
+                                
+                        </div>
+                    <div id="mce-responses" className="clear foot">
+                        <div className="response" id="mce-error-response" ></div>
+                        <div className="response" id="mce-success-response" ></div>
+                    </div>
+                
+                    <div className="optionalParent">
+                        <div className="clear foot">
+                            <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button"></input>
+                            <p><a href="http://eepurl.com/iwPweY" title="Mailchimp - email marketing made easy and fun"></a></p>
+                                
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
+            </div>
+
         {/* <!-- Form Section --> */}
         {/* <div> */}
         {/* {state == 2 ? (
